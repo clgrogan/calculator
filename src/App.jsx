2821e475f075e54e130184482a7736e2a4caa244
Curt Grogan <curtgrogan@outlook.com>
2020-03-04 09:36:44 -0500

    Deleted unused pages/components and references.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import Page from './pages/Page'
-import Page2 from './pages/Page2'
 import Calculator from './pages/Calculator'
 import NotFound from './pages/NotFound'
 
@@ -10,8 +8,6 @@ const App = () => {
     <Router>
       <Switch>
         <Route exact path="/" component={Calculator}></Route>
-        <Route exact path="/1" component={Page}></Route>
-        <Route exact path="/2" component={Page2}></Route>
         <Route path="*" component={NotFound}></Route>
       </Switch>
     </Router>
